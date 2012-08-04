8e24a7708492df607c38044d24b3d2dd3019edd9
Shintaro Kaneko <kaneshin0120@gmail.com>
2012-08-04 19:30:50 +0900

    Do refactoring

---
+++ assets/javascripts/canvas.js
@@ -0,0 +1,50 @@
+/* vim:set ts=8 sts=2 sw=2 tw=0:
+ * vim:set fdm=marker:
+ *
+ * File:        canvas.js
+ * Maintainer:  Shintaro Kaneko <kaneshin0120@gmail.com>
+ * Last Change: 04-Aug-2012.
+ */
+
+/*
+ * getCanvasById
+ */
+var getCanvasById = function(id) {
+  var canvas = {}
+    , el = document.getElementById(id)
+    ;
+  canvas = {
+      self: el
+    , context: el.getContext("2d")
+    , width: el.width
+    , height: el.height
+    , bg_color: "rgba(0,0,0,0)"
+  };
+  return canvas;
+};
+
+/*
+ * CanvasManager
+ */
+var CanvasManager = function(canvas) {
+  var that = {}
+    , context = canvas.context
+    , width = canvas.width
+    , height = canvas.height
+    , bg_color = canvas.bg_color
+    ;
+  that = {
+    clearCanvas: function() {
+      context.clearRect(0, 0, width, height);
+    },
+    fillCanvas: function(color) {
+      context.fillStyle = color || bg_color;
+      context.fillRect(0, 0, width, height);
+    },
+    setBackgroundColor: function(color) {
+      bg_color = color || "rgba(0,0,0,0)";
+    },
+  };
+  return that;
+};
+
